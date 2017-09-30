1ead19e62c68a13b48b1c1a0820a09fa9f972b7f
egunn <erica.gunn@gmail.com>
2017-09-30 07:24:01 -0400

    add rectangle grid

--- circleExample/index.js
+++ circleExample/index.js
@@ -1,7 +1,9 @@
 var svg = d3.select('svg');
 
-/* Your code goes here */
 
+/*////////////////////////////////////
+*  Make a ring of circles
+*/////////////////////////////////////
 
 var circleData = [];
 var angle = 0;
@@ -14,7 +16,6 @@ for (i=0; i<12; i++){
      circleData.push({ x: ringRadius * Math.sin(angle) + ringCenterX , y: ringRadius * Math.cos(angle) + ringCenterY , r:10});
 
     angle = angle + (2*Math.PI/12);
-    console.log(circleData);
 }
 
 
@@ -35,11 +36,49 @@ svg.selectAll('circle')
     .attr('fill', 'blue');
 
 
+/*////////////////////////////////////
+*  Add grid rectangles
+*/////////////////////////////////////
+
+var gridData = [];
+var stepX = 25;
+var stepY = 25;
+
+var rectData = [];
+
+//This one makes just a line of rectangles:
+/*
+for (i=0; i<8; i++){
+    rectData.push({x: i*stepX, y: 0, width: 10, height:10});
+}
+*/
+
+//to make a grid, use two for loops nested inside each other:
+for (i=0; i<8; i++){
+    for(j =0; j<8; j++){
+        rectData.push({x: i*stepX, y: j*stepY, width: 10, height:10});
+    }
+}
 
 
 
-
-
+svg.selectAll('rect')
+    .data(rectData)
+    .enter()
+    .append('rect')
+    .attr('x', function(d){
+        return d.x;
+    })
+    .attr('y', function(d){
+        return d.y;
+    })
+    .attr('width', function(d){
+        return d.width;
+    })
+    .attr('height', function(d){
+        return d.height;
+    })
+    .attr('fill', 'blue');
 
 
 
